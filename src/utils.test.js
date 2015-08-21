3fb6534fea81bf9a767ddb9acf8a376334cbadae
Mihai Rotaru <mihai.rotaru@gmx.com>
2015-08-21 16:34:12 +0100

    test wip

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,15 +1,37 @@
 var assert = require('assert');
+var nodeUtils = require('util');
 var utils = require('./utils.js');
 
-function test(f) {
-  var args = Array.prototype.slice.call(arguments, 1);
+function okMessage(msg) {
+  console.log('  \u001b[32;1m' + '√' + '\u001b[0m' + ' passed: ', msg);
+}
+function errorMessage(msg) {
+  console.log('  \u001b[31;1m' + 'x' + '\u001b[0m' + ' failed: ', msg);
+}
+var assert = require('assert');
+var monkeyPathed = ['equal', 'deepEqual' ];
+for (var i=0; i < monkeyPathed.length; ++i) {
+  var f = monkeyPathed[i];
+  var original = assert[f];
+  assert[f] = function() {
+    var args = Array.prototype.slice.call(arguments);
+    try {
+      original.apply(args);
+      okMessage(args);
+    } catch(err) {
+      errorMessage(err.message);
+    }
+  };
+}
+var originalThrow = assert.throws;
+assert['throws'] = function(a, b, c) {
   try {
-    f.apply(null,args);
-    console.log('passed: ', f.name, args);
+    originalThrow.call(null, a, b, c);
+    okMessage(arguments);
   } catch(err) {
-    console.log('failed: ' + err.message);
+    errorMessage(err.message);
   }
 }
 
-test(assert.deepEqual, utils.extractTags('a #foo #bar'), ['foo','bar']);
-test(assert.deepEqual, utils.extractTags('a #foo bar'), ['foo']);
+console.log('\u001b[37;1m' + __filename + '\u001b[0m');
+assert.deepEqual(utils.extractTags('a #foo bar'), ['foo']);
